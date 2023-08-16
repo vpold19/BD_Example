e8fb09dfc0ae865ae381184c27e21411c83290c4
vadim <vadimpoldolin19@gmail.com>
2023-08-16 23:24:11 +0200

    First commit

---
+++ src/main/java/com/skypro/test_demo_bd/TestDemoBdApplication.java
@@ -0,0 +1,13 @@
+package com.skypro.test_demo_bd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestDemoBdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestDemoBdApplication.class, args);
+	}
+
+}
